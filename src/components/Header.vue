a51e4e2df14176a8cca3e291bf6b1866ec095f73
Obez99 <giuliogx@live.it>
2021-11-11 14:49:37 +0100

    Adds static header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,20 @@
   <header>
     <img src="../assets/dc-logo.png" alt="header-logo" />
 
-    <nav><!--Nav goes here....<--></nav>
+    <nav>
+      <ul>
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">COLLECTIBLES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">SHOP</a></li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -11,3 +24,38 @@ export default {
   name: "Header",
 };
 </script>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+
+  ul {
+    list-style: none;
+    font-weight: bold;
+
+    li {
+      display: inline-block;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+        display: inline-block;
+
+        padding: 10px 20px;
+      }
+
+      &:hover {
+        color: #0282f9;
+      }
+    }
+  }
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+}
+</style>
