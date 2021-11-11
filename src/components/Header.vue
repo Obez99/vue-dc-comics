c90338cb69b3555e2eab7e6da2014639e26ccb6a
Obez99 <giuliogx@live.it>
2021-11-11 14:14:35 +0100

    Creates components

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <img src="../assets/dc-logo.png" alt="header-logo" />
+
+    <nav><!--Nav goes here....<--></nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
